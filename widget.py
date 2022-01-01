139a64178195daff26f5dcac8a55d7f4ed9d7e06
arielferdman <feariel@gmail.com>
2022-01-02 01:19:34 +0200

    working - besides documents field value empty at save

--- widget.py
+++ widget.py
@@ -1,20 +1,49 @@
-# This Python file uses the following encoding: utf-8
 import os
-from pathlib import Path
 import shutil
+import sqlite3
 import sys
-from PySide6 import QtGui
-from PySide6 import QtCore
+import json
+from enum import Enum
+from pathlib import Path
 
-from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow
+from PySide6 import QtCore, QtGui
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow
 
-from enum import Enum
 
-import sqlite3
+class SqlParts(Enum):
+    INSERT_START = 'insert into clients('
+    INSERT_MID = ') values ('
+    INSERT_END = ')'
 
-from pprint import pprint
+
+class Names(Enum):
+    COMMENTS_COLUMN_NAME = 'comments'
+    DB_NAME = 'simplecm.db'
+    MAIN_PYTHON_PROCESS = '__main__'
+
+
+class StringParts(Enum):
+    EMPTY_STRING = ''
+    COMMA_SPACE = ', '
+    QUESTION_MARK_COMMA_SPACE = '?, '
+
+
+class FileOperations(Enum):
+    READ = 'r'
+
+
+class DirNames(Enum):
+    CLIENT_DOCUMENTS_DIR = 'files'
+
+
+class DataFileNames(Enum):
+    INITIAL_FORM_DATA = 'initial_form_data.json'
+    CREATE_DB_QUERY = 'create_db_query.json'
+    COLUMN_NAMES = 'column_names.json'
+    DOCUMENT_ICON = 'file-line.svg'
+
 
 class Layouts(Enum):
     CREATE_CLIENT = 'form.ui'
@@ -24,49 +53,79 @@ class Layouts(Enum):
 class Main(QMainWindow):
     def __init__(self, parent=None):
         super(Main, self).__init__()
-        self.get_base_path()
-        self.get_ui_loader()
-        self.get_column_names()
-        self.get_base_dir()
-        self.get_documents_dir()
-        self.init_new_filepaths_property()
+        self.init_properties()
         self.load_ui()
 
-    def get_ui_loader(self):
+    def init_properties(self):
+        self.init_filesystem_props()
+        self.init_qt_props()
+        self.init_business_logic_props()
+
+    def init_business_logic_props(self):
+        self.set_up_db()
+        self.init_column_names()
+
+    def init_qt_props(self):
+        self.init_ui_loader()
+
+    def init_filesystem_props(self):
+        self.init_base_path()
+        self.init_base_dir()
+        self.init_documents_dir()
+        self.init_new_filepaths_property()
+        self.init_data_files_paths()
+
+    def init_data_files_paths(self):
+        self.init_column_names_file_path()
+        self.init_create_db_query_file_path()
+        self.init_initial_form_data_file_path()
+
+    def init_initial_form_data_file_path(self):
+        self.initial_form_data_file_path = os.path.join(
+            self.base_dir, DataFileNames.INITIAL_FORM_DATA.value)
+
+    def init_create_db_query_file_path(self):
+        self.create_db_query_file_path = os.path.join(
+            self.base_dir, DataFileNames.CREATE_DB_QUERY.value)
+
+    def init_column_names_file_path(self):
+        self.column_names_file_path = os.path.join(
+            self.base_dir, DataFileNames.COLUMN_NAMES.value)
+
+    def init_ui_loader(self):
         self.loader = QUiLoader()
 
     def create_client_document_folder(self):
         self.create_client_documents_folder_name()
         self.create_client_document_folder_path()
-        os.mkdir(self.client_documents_folder_path)
+        if not os.path.exists(self.client_documents_folder_path):
+            os.mkdir(self.client_documents_folder_path)
 
     def create_client_document_folder_path(self):
-        self.client_documents_folder_path = os.path.join(self.base_dir, self.documents_dir, self.client_documents_folder_name)
+        self.client_documents_folder_path = os.path.join(
+            self.base_dir, self.documents_dir, self.client_documents_folder_name)
 
     def create_client_documents_folder_name(self):
-        self.client_documents_folder_name = f'{self.form_data[0]}-{self.form_data[1]}-{self.form_data[2]}'
+        self.client_documents_folder_name = \
+            f'{self.form_data[0]}-{self.form_data[1]}-{self.form_data[2]}'
 
     def init_new_filepaths_property(self):
-        self.moved_filenames = []
+        self.full_moved_file_paths = []
 
-    def get_documents_dir(self):
-        self.documents_dir = 'files'
+    def init_documents_dir(self):
+        self.documents_dir = DirNames.CLIENT_DOCUMENTS_DIR.value
 
-    def get_base_dir(self):
+    def init_base_dir(self):
         self.base_dir = os.getcwd()
 
-    def get_column_names(self):
-        self.columns = [
-            'first_name',
-            'last_name',
-            'passport_no',
-            'national_id',
-            'address',
-            'employer1',
-            'employer2',
-            'comments',
-            'documents'
-        ]
+    def init_column_names(self):
+        self.columns = self.load_json(self.column_names_file_path)
+
+    def load_json(self, filename):
+        data = None
+        with open(filename, FileOperations.READ.value) as in_file:
+            data = json.loads(in_file.read())
+        return data
 
     def load_ui(self):
         self.get_screen_file_path(Layouts.CREATE_CLIENT)
@@ -74,11 +133,7 @@ class Main(QMainWindow):
         self.close_screen_ui_file()
         self.get_columns_properties()
         self.create_additional_widgets()
-        
-        # for dev only - remove after
         self.fill_in_form()
-        # for dev only - remove after
-
         self.add_icon_to_button()
 
     def load_screen(self, layout):
@@ -111,22 +166,17 @@ class Main(QMainWindow):
     def get_root_dir_file_path(self, filename):
         self.path = os.path.join(self.base_path, filename.value)
 
-    def get_base_path(self):
+    def init_base_path(self):
         self.base_path = os.fspath(Path(__file__).resolve().parent)
 
     def add_icon_to_button(self):
-        self.documents_button.setIcon(QtGui.QIcon('file-line.svg'))
+        self.documents_button.setIcon(QtGui.QIcon(DataFileNames.DOCUMENT_ICON.value))
         self.documents_button.setIconSize(QtCore.QSize(24, 24))
 
     def fill_in_form(self):
-        self.first_name.setText('אריאל')
-        self.last_name.setText('פרדמן')
-        self.passport_no.setText('350186548')
-        self.national_id.setText('066423872')
-        self.address.setText('אחד העם 1 ב רחובות 7626103')
-        self.employer1.setText('לרה יעקב')
-        self.employer2.setText('עזרה סוכנות כוח אדם בע"מ')
-        self.comments.setText('סוכם שהתוכנה תסופק הערב')
+        initial_form_data = self.load_json(self.initial_form_data_file_path)
+        for key, value in initial_form_data.items():
+            getattr(self, key).setText(value)
 
     def select_documents(self):
         self.get_documents_paths()
@@ -135,10 +185,7 @@ class Main(QMainWindow):
         self.documents_paths = QFileDialog.getOpenFileNames()[0]
 
     def update_documents(self):
-        self.documents.setText(''.join(
-            os.path.join(self.client_documents_folder_path, filename)
-            for filename in self.get_documents_filenames()
-        ))
+        self.documents.setText(json.dumps(self.full_moved_file_paths))
 
     def get_documents_filenames(self):
         return [self.get_filename(file_path) for file_path in self.documents_paths]
@@ -148,13 +195,16 @@ class Main(QMainWindow):
 
     def move_files(self):
         for file_path in self.documents_paths:
-            shutil.move(file_path, self.client_documents_folder_path)
+            full_new_file_path = os.path.join(
+                self.client_documents_folder_path, file_path)
+            shutil.move(file_path, full_new_file_path)
+            self.full_moved_file_paths.append(full_new_file_path)
 
     def create_client(self):
-        self.set_up_db()
-        self.save_client_to_db()
+        self.get_form_data()
         self.create_client_document_folder()
         self.move_client_documents()
+        self.save_client_to_db()
         self.close_sqlite_connection()
         self.switch_layout(Layouts.CLIENT_LIST)
 
@@ -173,7 +223,6 @@ class Main(QMainWindow):
         self.update_documents()
 
     def create_query(self):
-        self.get_form_data()
         self.get_additional_query_params()
 
     def set_up_db(self):
@@ -184,49 +233,38 @@ class Main(QMainWindow):
         self.con.close()
 
     def insert_new_client_to_db(self):
-        query = f'insert into clients({self.columns_seperator.join(self.columns)}) values ({self.values_placeholders})'
+        query = SqlParts.INSERT_START.value + self.columns_seperator.join(
+            self.columns) + SqlParts.INSERT_MID.value + self.values_placeholders \
+            + SqlParts.INSERT_END.value
 
         self.cur.execute(query, self.form_data)
         self.con.commit()
 
     def create_table_if_doesnt_exist(self):
-        query = \
-            '''
-            create table if not exists clients(
-            id integer primary key autoincrement,
-            first_name text,
-            last_name text,
-            passport_no text,
-            national_id text,
-            address text,
-            employer1 text,
-            employer2 text,
-            comments text,
-            documents text
-            )
-        '''
+        query = self.load_json(self.create_db_query_file_path)
         self.cur.execute(query)
         self.con.commit()
 
     def get_additional_query_params(self):
-        self.values_placeholders = ('?, ' * (len(self.columns)))[:-2]
-        self.columns_seperator = ', '
+        self.values_placeholders = (
+            StringParts.QUESTION_MARK_COMMA_SPACE.value * (len(self.columns)))[:-2]
+        self.columns_seperator = StringParts.COMMA_SPACE.value
 
     def get_form_data(self):
         self.form_data = [
             getattr(self, column_name).text()
-            if column_name != 'comments'
+            if column_name != Names.COMMENTS_COLUMN_NAME.value
             else getattr(self, column_name).toPlainText()
             for column_name in self.columns
         ]
 
     def get_sqlite_connection(self):
-        self.con = sqlite3.connect('simplecm.db')
+        self.con = sqlite3.connect(Names.DB_NAME.value)
         self.cur = self.con.cursor()
 
 
 def start_main_program(__name__, Main):
-    if __name__ == "__main__":
+    if __name__ == Names.MAIN_PYTHON_PROCESS.value:
         app = QApplication(sys.argv)
         main = Main()
         main.setMinimumWidth(800)
@@ -234,5 +272,6 @@ def start_main_program(__name__, Main):
         main.show()
         dir(main)
         sys.exit(app.exec())
+
 
 start_main_program(__name__, Main)
